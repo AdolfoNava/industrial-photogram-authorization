8122f4799c8f166d67a30e3662cfb43a955a5e5b
AdolfoNava <adolfonava1990@gmail.com>
2024-07-18 11:35:17 -0500

    Remove photo comments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -76,12 +76,6 @@ class PhotosController < ApplicationController
         raise Pundit::NotAuthorizedError, "Not allowed"
       end
     end
-    # def ensure_current_user_is_owner
-    #   if current_user != @photo.owner
-    #     redirect_back fallback_location: root_url, alert: "You're not authorized for that."
-    #   end
-    # end
-    # Only allow a list of trusted parameters through.
     def photo_params
       params.require(:photo).permit(:image, :comments_count, :likes_count, :caption, :owner_id)
     end
